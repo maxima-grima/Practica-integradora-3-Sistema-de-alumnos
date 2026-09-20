4cfc1e20da171830554574cf1f031ca9fdeda6fa
maximagrima <mgrima@mail.austral.edu.ar>
2026-09-20 18:51:11 -0300

    Etapa 2: constructor de Alumno

--- SistemaAlumnos/Alumno.cs
+++ SistemaAlumnos/Alumno.cs
@@ -1,10 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿namespace SistemaAlumnos;
 
-namespace SistemaAlumnos
+public class Alumno
 {
-    internal class Alumno
+    // Etapa 1
+    public string Nombre { get; set; } = "";
+    public int Legajo { get; set; }
+    public double Nota1 { get; set; }
+    public double Nota2 { get; set; }
+    // Etapa 2
+    public Alumno(string nombre, int legajo, double nota1, double nota2)
     {
+        Nombre = nombre;
+        Legajo = legajo;
+        Nota1 = nota1;
+        Nota2 = nota2;
     }
 }
--- SistemaAlumnos/Program.cs
+++ SistemaAlumnos/Program.cs
@@ -1,6 +1,6 @@
 ﻿using SistemaAlumnos;
 
-// Etapa 1
+/* Etapa 1
 Alumno alumno1 = new Alumno();
 alumno1.Nombre = "Ana Pérez";
 alumno1.Legajo = 1234;
@@ -12,9 +12,15 @@ alumno2.Nombre = "Juan Gómez";
 alumno2.Legajo = 5678;
 alumno2.Nota1 = 9;
 alumno2.Nota2 = 8;
+*/
+// Etapa 2
+Alumno alumno1 = new Alumno("Ana Pérez", 1234, 7, 7);
+Alumno alumno2 = new Alumno("Juan Gómez", 5678, 5, 8);
 
 Console.WriteLine($"{alumno1.Nombre} - {alumno1.Legajo}");
 Console.WriteLine($"{alumno2.Nombre} - {alumno2.Legajo}");
 alumno1.Nombre = "Ana María Pérez";
 Console.WriteLine(alumno1.Nombre);
 Console.WriteLine(alumno2.Nombre);
+// Etapa 2 - punto 3
+Alumno a3 = new Alumno();
