02fde5cd4f2070b227e8cf0a0247269bfd4d94e8
maximagrima <mgrima@mail.austral.edu.ar>
2026-09-20 17:37:25 -0300

    Etapa 1: clase Alumno

--- SistemaAlumnos/Program.cs
+++ SistemaAlumnos/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using SistemaAlumnos;
+
+// Etapa 1
+Alumno alumno1 = new Alumno();
+alumno1.Nombre = "Ana Pérez";
+alumno1.Legajo = 1234;
+alumno1.Nota1 = 7;
+alumno1.Nota2 = 7;
+
+Alumno alumno2 = new Alumno();
+alumno2.Nombre = "Juan Gómez";
+alumno2.Legajo = 5678;
+alumno2.Nota1 = 9;
+alumno2.Nota2 = 8;
+
+Console.WriteLine($"{alumno1.Nombre} - {alumno1.Legajo}");
+Console.WriteLine($"{alumno2.Nombre} - {alumno2.Legajo}");
+alumno1.Nombre = "Ana María Pérez";
+Console.WriteLine(alumno1.Nombre);
+Console.WriteLine(alumno2.Nombre);
